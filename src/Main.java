f2759182d39d8cc1dcb7084896bda36cab0c45a1
Dennis Roggan <d.r@fu-berlin.de>
2016-11-19 16:34:20 +0100

    Print error message, if called with too many command line argument.

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,19 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		String filename="";
-		if(args.length>=1) filename = args[0];
-		else {
+		if (args.length == 1) {
+			filename = args[0];
+		} else if (args.length > 1) {
+			System.out.println("Error: Too many Arguments.");
+			System.exit(-1);
+		} else {
 			System.out.println("Enter file name: ");
 			Scanner in = new Scanner(System.in);
 			filename = in.next();
 			in.close();
 		}
 		  
-	    LexScanner ls = new LexScanner(filename);
+		LexScanner ls = new LexScanner(filename);
 		
 		Token t=null;
 		int line = 0;
