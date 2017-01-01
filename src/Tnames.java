e9af32560ca5b5253a9ddd8ad667c8a1d66f97bf
Dennis Roggan <d.r@fu-berlin.de>
2017-01-01 20:33:54 +0100

    Added Tilde

--- src/Tnames.java
+++ src/Tnames.java
@@ -10,7 +10,8 @@ public enum Tnames {
 	LE("<=", "OPERATOR"),
 	ASSIGN("<-", "OPERATOR"),
 	EQ("=", "OPERATOR"),
-	
+	TILDE("~", "OPERATOR"),
+
 	//Keywords:
 	CLASS("class", "KEYWORD"),
 	INHERITS("inherits", "KEYWORD"),
